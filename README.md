be31b059532ffca0923922db22f8708bd6beb5b4
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-05-17 18:08:25 +0200

    sveltauriwind init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proxima
+
+Tauri
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const spiral: string = 'out';
+	$: console.log('%cspiral', 'color:red', spiral);
+</script>
+
+<div
+	class="h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-tr from-pink-900 to-stone-900"
+>
+	<h1 class="text-stone-400">Proxima</h1>
+	<h1 class="text-fuchsia-700">Tauri</h1>
+</div>
